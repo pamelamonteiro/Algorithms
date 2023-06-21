206e36f711bfb6014151d97feb6352db1776ea6d
pamelamonteiro <pamelamonteiro_@hotmail.com>
2023-06-21 00:40:55 -0300

    segundo requisito

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,10 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(Exception):
+        encrypt_message(78, "caleidoscopio")
+    assert encrypt_message("caleidoscopio", 78) == "oipocsodielac"
+    assert encrypt_message("caleidoscopio", 12) == "o_ipocsodielac"
+    assert encrypt_message("caleidoscopio", 3) == "lac_oipocsodie"
