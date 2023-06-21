c498cd91a74395c9bd28fd5ae1ca35bd10c8241a
pamelamonteiro <pamelamonteiro_@hotmail.com>
2023-06-21 06:07:32 -0300

    quinto requisito corrigido

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -3,6 +3,8 @@ def find_duplicate(nums):
         return False
     sorted(nums)
     for n in range(1, len(nums)):
+        if isinstance(nums[n], int) is False:
+            return False
         if nums[n] <= 0:
             return False
         if nums[n] == nums[n - 1]:
