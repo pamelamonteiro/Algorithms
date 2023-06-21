b00e90aa6ed17993612e8430f7dcc80320663e55
pamelamonteiro <pamelamonteiro_@hotmail.com>
2023-06-21 02:12:31 -0300

    sexto requisito

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == '':
+        return False
+    txt = word[::-1]
+    index = 0
+    array_size = len(word) - 1
+    while index <= array_size:
+        if word[index] != txt[index]:
+            return False
+        index += 1
+    return True
+
+# https://www.w3schools.com/python/python_howto_reverse_string.asp
+# https://www.freecodecamp.org/news/typeerror-string-indices-must-be-integers-how-to-fix-in-python/
