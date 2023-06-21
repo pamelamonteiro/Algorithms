4fcd6dce04db5abd6ad603538c3e691f0a6e9680
pamelamonteiro <pamelamonteiro_@hotmail.com>
2023-06-21 05:58:11 -0300

    quinto requisito

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if nums == "":
+        return False
+    sorted(nums)
+    for n in range(1, len(nums)):
+        if nums[n] <= 0:
+            return False
+        if nums[n] == nums[n - 1]:
+            return nums[n]
+    return False
+
+# https://opensource.com/article/23/1/fix-indexerror-python#:~:text=The%20only%20solution%20to%20fix,()%20an%20len()%20functions.
+# https://www.w3schools.com/python/ref_func_isinstance.asp
