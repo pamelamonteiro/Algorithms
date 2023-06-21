9f6bb1f855e4ba4304df67dcbd142a251f10e0e5
pamelamonteiro <pamelamonteiro_@hotmail.com>
2023-06-21 00:18:06 -0300

    corrigindo validacao primeiro requisito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    if target_time is None:
+    try:
+        students = 0
+        for login, logout in permanence_period:
+            if login <= target_time <= logout:
+                students += 1
+        return students
+    except TypeError:
         return None
-    
-    if permanence_period is not int:
-        return None
-    
-    students = 0
-    for login, logout in permanence_period:
-        if login <= target_time <= logout:
-            students += 1
-    raise NotImplementedError
